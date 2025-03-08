b69ad92623f7daa1831a147d64010de0918d9cd8
bobo <bobodai515@163.com>
2025-03-08 11:53:41 +0800

    feat: add chain control

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,16 +1,37 @@
 use core::block_chain;
+use std::io;
 
 fn main() {
     // 新建链，难度系数为2算的会比较快，过大会算的很慢，为 -1 不进行工作量证明
     let mut bc = block_chain::BlockChain::new_block_chain(4);
 
-    bc.add_block("a -> b: 5btc".to_string());
+    loop {
+        println!("🔹 请输入指令 (或输入 'exit' 退出): ");
 
-    bc.add_block("c -> d: 5btc".to_string());
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let data = input.trim().to_string();
+        let data = data.splitn(2, " ").collect::<Vec<&str>>();
 
-    for b in bc.blocks {
-        println!("++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        println!("{:#?}", b);
-        println!("");
+        if data[0] == "exit" {
+            println!("👋  退出区块链...");
+            break;
+        }
+
+        if data[0] == "add" {
+            println!("⛏️ 正在创建区块");
+            bc.add_block(data[1].to_string());
+            println!("⛏️ 创建完成");
+            continue;
+        }
+
+        if data[0] == "list" {
+            for b in &bc.blocks {
+                println!("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                println!("{:#?}", b);
+            }
+            continue;
+        }
+        println!("❌ 请输入 add 加交易信息添加区块，输入list查看当前链。");
     }
 }
